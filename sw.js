92d0b828814a610d56dc2f1678c24aa781bb73a2
Victoria Dahlquist <wictooria@gmail.com>
2018-01-21 17:30:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,6 +19,7 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event) {
   // Perform install steps
+  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
