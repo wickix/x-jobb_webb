9de889387e23a4539ccbb5ad1f7f4207789caf8a
Victoria Dahlquist <wictooria@gmail.com>
2018-01-21 17:56:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -41,3 +41,21 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = [];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
