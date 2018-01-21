4c5d83d077e5905dd009a8ef6802fc9a81309867
Victoria Dahlquist <wictooria@gmail.com>
2018-01-21 17:49:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -30,14 +30,28 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
+
     caches.match(event.request)
       .then(function(response) {
         // Cache hit - return response
+        var fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+        );
+        //return fetch(fetchRequest);
+        
         if (response) {
           return response;
         }
-        return fetch(event.request);
+            
       }
     )
+    
+    
   );
 });
